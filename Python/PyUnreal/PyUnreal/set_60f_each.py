91e30f804d45a30293c32cd81d9295578b8228b5
Mino <38447110+MINOMINOMI@users.noreply.github.com>
2022-01-21 12:59:47 +0900

    AnimSequence ImportData edit Script

---
+++ Python/PyUnreal/PyUnreal/set_60f_each.py
@@ -0,0 +1,16 @@
+from pickle import FALSE
+import unreal
+
+ar_asset_lists = unreal.EditorUtilityLibrary.get_selected_assets()
+ar_selected_0 = ar_asset_lists[0]
+
+
+for each in ar_asset_lists :
+    import_data_selected = each.get_editor_property("asset_import_data")
+
+    frame_rate_data = import_data_selected.get_editor_property("custom_sample_rate")
+    using_default_sample_rate = import_data_selected.get_editor_property("use_default_sample_rate")
+    
+    if using_default_sample_rate : 
+        import_data_selected.set_editor_property("use_default_sample_rate", False)
+        import_data_selected.set_editor_property("custom_sample_rate", 60)
