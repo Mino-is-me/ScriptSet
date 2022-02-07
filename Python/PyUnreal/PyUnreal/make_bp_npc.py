8faa0f762f49fba227d7196d956e25581aaf5072
Mino <38447110+MINOMINOMI@users.noreply.github.com>
2022-02-07 17:30:55 +0900

    AnimInstance 할당 작업

--- Python/PyUnreal/PyUnreal/make_bp_npc.py
+++ Python/PyUnreal/PyUnreal/make_bp_npc.py
@@ -1,6 +1,17 @@
 import unreal 
 #import re
 
+def get_bp_c_by_name(__bp_dir:str):
+    __bp_c = __bp_dir + '_C' 
+    return __bp_c 
+
+def get_bp_mesh_comp (__bp_c:str) :
+    #source_mesh = ue.load_asset(__mesh_dir)
+    loaded_bp_c = unreal.EditorAssetLibrary.load_blueprint_class(__bp_c)
+    bp_c_obj = unreal.get_default_object(loaded_bp_c)
+    loaded_comp = bp_c_obj.get_editor_property('Mesh')
+    return loaded_comp
+
 
 
 ar_asset_lists = []
@@ -55,4 +66,25 @@ Skeleton = ar_asset_lists[0].skeleton
 asset_bp = unreal.EditorAssetLibrary.duplicate_asset(BaseBP,BPPath)
 AnimBP = unreal.EditorAssetLibrary.duplicate_asset(BaseAnimBP,AnimBPPath)
 AnimBP.set_editor_property("target_skeleton", Skeleton)
+
+# BPPath_C = get_bp_c_by_name(BPPath)
+# BP_mesh_comp = get_bp_mesh_comp_by_name(BPPath_C, 'Mesh')
+
+# BP_mesh_comp.set_editor_property('skeletal_mesh',ar_asset_lists[0])
+# BP_mesh_comp.set_editor_property('anim_class', loaded_a)
 # '''BP setting end'''
+
+# BP Component Setting Start #
+_bp_ = unreal.EditorAssetLibrary.get_path_name_for_loaded_asset(asset_bp)
+_abp_ = unreal.EditorAssetLibrary.get_path_name_for_loaded_asset(AnimBP)
+
+_bp_c = get_bp_c_by_name(_bp_) 
+_abp_c = get_bp_c_by_name(_abp_)
+
+loaded_abp = unreal.EditorAssetLibrary.load_blueprint_class(_abp_c)
+bp_mesh_comp = get_bp_mesh_comp(_bp_c)
+
+bp_mesh_comp.set_editor_property('skeletal_mesh',ar_asset_lists[0])
+bp_mesh_comp.set_editor_property('anim_class', loaded_abp)
+
+# BP Component Setting End #
