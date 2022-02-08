3c1ab7b6d593737ca45018eb2e63bfeccad9b4b0
Mino <38447110+MINOMINOMI@users.noreply.github.com>
2022-02-08 13:56:33 +0900

    주석추가

--- Python/PyUnreal/PyUnreal/library/lib_narrs.py
+++ Python/PyUnreal/PyUnreal/library/lib_narrs.py
@@ -59,7 +59,8 @@ def get_bp_mesh_comp_by_name (__bp_c:str, __seek_comp_name:str) :
     loaded_comp = bp_c_obj.get_editor_property(__seek_comp_name)
     return loaded_comp
     #인풋으로 받는 __bp_c 아규먼트 '/direc/*_Bluperint.*_Blueprint_c' << 와 같은 포맷으로 받아야함
-    #__seek_comp_name 아규먼트 동작 안하면 아래의 함수를 사용하세요.
+    ##asset_path를 str로 끌어왔을 경우 상단의 get_bp_c_by_name 함수 이용하여 class로 한번 더 래핑해주세요.
+    ###__seek_comp_name 아규먼트 동작 안하면 아래의 함수를 사용하세요.
 
 
 def get_bp_instance (__bp_c:str) :
